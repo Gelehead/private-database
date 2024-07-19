e0df595fa728767a212590b904e8bd969ecf5fe8
Gelehead <oscar.lavolet@gmail.com>
2024-07-19 19:04:31 +0200

    small advancement on the oath project

--- miniProjects/oath cards collector/source/cardSearch.py
+++ miniProjects/oath cards collector/source/cardSearch.py
@@ -7,39 +7,62 @@ from cmath import log10
 def get3digitFormat(num):
     return f"{num:03d}"
 
+def text_to_image_link(text):
+    return text.replace(" ", "%20")
+
+# ACTUAL DATA GETTERS
+def get_content(p_soup):
+    """ transforms effect description from a card to understandable text
+
+    Args:
+        p_soup (String): HTML p container with text and possibly images
+
+    Returns:
+        String: readable effect description
+    """
+    text_parts = []
+
+    for content in p_soup.contents:
+        if content.name == 'img':
+            text_parts.append(dices_adapter(content['title']))
+        else:
+            text_parts.append(content.strip())
+    return ' '.join(text_parts)
+
+def dices_adapter(dice):
+    str(dice).replace(" ", "")
+    match dice :
+        case "diceb":
+            return "defense die"
+
+
+# TODO : automatic page making
+def make_web_page(card_data):
+    a=1
+
 # note : this has been made in summer 2024, the game may have had some extensions since then ( if yes, add "extension" column )
-def allCard(writeAllInFile=False):
+def allCards(writeAllInFile=False):
     cards = []
     for i in range(1, 270):
         cards.append(singleCard("https://oathcards.seiyria.com/card/OATH-" + get3digitFormat(i) + "?q=&d=images&s=name&b=asc&p=0"))
-    df = pd.DataFrame(cards, columns=['Name', 'Faction', 'Type', 'Effect', 'FAQ text', 'image'])
+    df = pd.DataFrame(cards, columns=['Title', 'Faction', 'Type', 'Effect', 'FAQ text', 'image'])
 
 def singleCard(url, writeInFile=False):
     response = requests.get(url)
     soup = BeautifulSoup(response.content, 'html.parser')
     
+    print(soup)
+    
     data = []
-    image = soup.find('ion-img').find('img').get('src')
-    print(image)
-    for row in soup.find('table').find('tbody').select('tr'):
-        moveName = cell.find('td', class_='cell-name').find('a', class_='ent-name').get_text()
-        moveType = cell.find('td', class_='cell-icon').find('a').get_text()
-        moveCat = cell.find('td', class_='cell-icon text-center')['data-sort-value']
-        movePower_Acc_PP_ProbofsecEffect = []
-        for cellNum in cell.select('td.cell-num'):
-            movePower_Acc_PP_ProbofsecEffect.append(cellNum.get_text())
-        moveEff = cell.find('td', class_='cell-long-text')
-        moves.append([moveName, moveType, moveCat,\
-                      '-' if movePower_Acc_PP_ProbofsecEffect[0] == '—' else movePower_Acc_PP_ProbofsecEffect[0], \
-                      '-' if movePower_Acc_PP_ProbofsecEffect[1] == '—' else movePower_Acc_PP_ProbofsecEffect[1], \
-                      '-' if movePower_Acc_PP_ProbofsecEffect[2] == '—' else movePower_Acc_PP_ProbofsecEffect[3], \
-                      moveEff, \
-                      '-' if movePower_Acc_PP_ProbofsecEffect[3] == '—' else movePower_Acc_PP_ProbofsecEffect[3]])
-
-    # did not get what the dataFrame is for
-    df = pd.DataFrame(moves, columns=['Name', 'Type', 'Category', 'Power', 'Accuracy', 'PP', 'Effect', 'Prob of effect'])
-
+    title = soup.find('h1').get_text()
+    image = 'https://ledercardcdn.seiyria.com/cards/oath/en-US/' + text_to_image_link(title) + '.webp'
+    effect = get_content(soup.find('app-card-text').find('p'))
+    
+    # create simple web page layout for the cards 
     if writeInFile:
-        df.to_csv("AllMoves.csv", index=False)
+        f = open(title + ".html", "w")
+        f.write(make_web_page(data))
         
-    return df
+    return data
+
+allCards()
